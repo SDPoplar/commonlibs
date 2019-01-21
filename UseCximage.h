3fea2f7f119c3091af0b2ed15acc2421a5465d08
SDPoplar <741027037@qq.com>
2019-01-21 10:54:17 +0800

    cximage

---
+++ UseCximage.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "cximage/ximage.h"
+
+#pragma comment( lib, "cximage\\lib\\libdcr.lib" )
+#pragma comment( lib, "cximage\\lib\\cximage.lib" )
+#pragma comment( lib, "cximage\\lib\\mng.lib" )
+#pragma comment( lib, "cximage\\lib\\Jpeg.lib" )
+#pragma comment( lib, "cximage\\lib\\png.lib" )
+#pragma comment( lib, "cximage\\lib\\Tiff.lib" )
+#pragma comment( lib, "cximage\\lib\\zlib.lib" )
+#pragma comment( lib, "cximage\\lib\\jasper.lib" )
